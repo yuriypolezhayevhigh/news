7dc4e6c05c31448bd6435343f121ec501ab37d09
swaty007 <swaty0007@gmail.com>
2020-07-15 13:55:59 +0300

    0.11 fixes Regexp

--- parser/parserWP.js
+++ parser/parserWP.js
@@ -23,7 +23,7 @@ const mysql    = require('mysql'),
 class parserWP {
   constructor () {
     this.limit = 10
-    this.offset = 349
+    this.offset = 392
     this.total = null
     this.languages = [
       'ru',
